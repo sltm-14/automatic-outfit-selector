24cefc58cfbc1b122eb42553d1354748eafda99f
sltm-14 <ie693242@iteso.mx>
2020-10-10 23:50:57 -0500

    tops images added

--- automatic-outfit-selector.py
+++ automatic-outfit-selector.py
@@ -1,10 +1,33 @@
+import os
 import tkinter as tk              # To create the GUI
 from PIL import Image, ImageTk  # To work with images
 #from playsound import playsound # To play with oudio
 
+WINDOW_TITLE = "My Wardrobe"
+
+WINDOW_WIDTH  = 500
+WINDOW_HEIGHT = 220
+
+# Store all the tops into a file we can access and skip hidden files
+ALL_TOPS = [str('tops/') + imagefile for imagefile in os.listdir('tops/') if not imagefile.startswith('.')]
+
 class WardrobeApp:
     def __init__(self,root):
-        self.root = root
+        self.root = root                         # Root will be a tkinter object
+
+        self.top_images = ALL_TOPS               # Show top image in the window
+
+        self.top_image_pat = self.top_images[0]  # Save single top
+
+        self.tops_frame = tk.Frame(self.root)     # Create and add top image into Frame
+
+
+        self.create_background() # Create background
+
+    def create_background(self):
+        self.root.title(WINDOW_TITLE) # Add title to window
+        self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT)) #size of window
+
 
 
 root = tk.Tk()

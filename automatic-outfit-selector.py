099a43894ba3e591d05723e4980f6f37dddc603b
sltm-14 <ie693242@iteso.mx>
2020-10-11 22:18:07 -0500

    Tops displayed

--- automatic-outfit-selector.py
+++ automatic-outfit-selector.py
@@ -20,10 +20,13 @@ class WardrobeApp:
 
         self.top_images = ALL_TOPS               # Show top image in the window
 
-        self.top_image_pat = self.top_images[0]  # Save single top
+        self.top_image_path = self.top_images[0]  # Save single top
 
         self.tops_frame = tk.Frame(self.root)    # Create and add top image into Frame (first image to be displayed)
+        self.top_image_label = self.create_photo(self.top_image_path,self.tops_frame) # to pack this image into the tops frame and the pack the tops frame into the root
+        self.top_image_label.pack(side=tk.TOP) # add it to pack
 
+        self.tops_frame.pack(fill=tk.BOTH,expand=tk.YES)
 
         self.create_background() # Create background
 
@@ -31,17 +34,17 @@ class WardrobeApp:
         self.root.title(WINDOW_TITLE) # Add title to window
         self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT)) #size of window
 
-        def create_photos(self):
-            image_file = Image.open(image_path)
-            image_resize = imge_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-            tk_photo = imageTk.PhotoImage(image_resize)
-            image_label = tk.Label(frame, image = tk_photo, anchor = tk.Center)
-            #weird tkinter quirk
-            image_label.image = tk_photo
+    def create_photo(self,image_path,frame):
+        image_file = Image.open(image_path)
+        image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+        tk_photo = ImageTk.PhotoImage(image_resized)
+        image_label = tk.Label(frame, image = tk_photo, anchor = tk.CENTER)
+        #weird tkinter quirk
+        image_label.image = tk_photo
 
-            #we can add later
-            return image_label
-            #7:19
+        #we can add later
+        return image_label
+        #7:19
 
 root = tk.Tk()
 app = WardrobeApp(root)
